8e14dbec470ec2860eb192cb78e88d2c56ed38d2
Roy Ben Yosef <roy.benyosef@cyberark.com>
2020-06-29 15:34:43 +0300

    Working template

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from iot_cdk_poc.iot_cdk_poc_stack import IotCdkPocStack
 
 
 app = core.App()
-IotCdkPocStack(app, "iot-cdk-poc")
+IotCdkPocStack(app, "roy-iot-cdk-poc")
 
 app.synth()
--- iot_cdk_poc/iot_cdk_poc_stack.py
+++ iot_cdk_poc/iot_cdk_poc_stack.py
@@ -1,9 +1,105 @@
 from aws_cdk import core
-
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_iot as iot
 
 class IotCdkPocStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        self.provisioning_role = iam.Role(
+            self, "AwsIotProvisioningRole", 
+            assumed_by=iam.ServicePrincipal("iot.amazonaws.com"), 
+            inline_policies={
+                "AwsIotProvisioningPolicy":
+                    iam.PolicyDocument(statements=[
+                        iam.PolicyStatement(
+                            actions=[
+                                "iot:AddThingToThingGroup",
+                                "iot:AttachPrincipalPolicy",
+                                "iot:AttachThingPrincipal",
+                                "iot:CreateCertificateFromCsr",
+                                "iot:CreatePolicy",
+                                "iot:CreateThing",
+                                "iot:DescribeCertificate",
+                                "iot:DescribeThing",
+                                "iot:DescribeThingGroup",
+                                "iot:DescribeThingType",
+                                "iot:DetachThingPrincipal",
+                                "iot:GetPolicy",
+                                "iot:ListPolicyPrincipals",
+                                "iot:ListPrincipalPolicies",
+                                "iot:ListPrincipalThings",
+                                "iot:ListThingGroupsForThing",
+                                "iot:ListThingPrincipals",
+                                "iot:RegisterCertificate",
+                                "iot:RegisterThing",
+                                "iot:RemoveThingFromThingGroup",
+                                "iot:UpdateCertificate",
+                                "iot:UpdateThing",
+                                "iot:UpdateThingGroupsForThing",
+                                "iot:AddThingToBillingGroup",
+                                "iot:DescribeBillingGroup",
+                                "iot:RemoveThingFromBillingGroup"
+                            ],
+                            resources=["*"],
+                            effect=iam.Effect.ALLOW,
+                        )
+                    ])
+            })
+
+        template_body = get_template_body(device_type='YaronN_DeviceType1', policy_name="YaronN_Device1-Policy")
+        self.provisioning_template = \
+            iot.CfnProvisioningTemplate(scope=self, 
+                                        id='AWSIoTProvisioningTemplate',
+                                        provisioning_role_arn=self.provisioning_role.role_arn,
+                                        template_body=template_body,
+                                        enabled=True,
+                                        template_name='IotProvisioningTemplateTest1',
+                                        pre_provisioning_hook=None)
+
+
+def get_template_body(device_type: str, policy_name: str) -> str:
+    return f'''{{
+    "Parameters": {{
+        "SerialNumber": {{
+        "Type": "String"
+        }},
+        "AWS::IoT::Certificate::Id": {{
+        "Type": "String"
+        }}
+    }},
+    "Resources": {{
+        "certificate": {{
+        "Properties": {{
+            "CertificateId": {{
+            "Ref": "AWS::IoT::Certificate::Id"
+            }},
+            "Status": "Active"
+        }},
+        "Type": "AWS::IoT::Certificate"
+        }},
+        "policy": {{
+        "Properties": {{
+            "PolicyName": "{policy_name}"
+        }},
+        "Type": "AWS::IoT::Policy"
+        }},
+        "thing": {{
+        "OverrideSettings": {{
+            "AttributePayload": "MERGE",
+            "ThingGroups": "DO_NOTHING",
+            "ThingTypeName": "REPLACE"
+        }},
+        "Properties": {{
+            "AttributePayload": {{}},
+            "ThingGroups": [],
+            "ThingName": {{
+            "Ref": "SerialNumber"
+            }},
+            "ThingTypeName": "{device_type}"
+        }},
+        "Type": "AWS::IoT::Thing"
+        }}
+    }}
+    }}'''
